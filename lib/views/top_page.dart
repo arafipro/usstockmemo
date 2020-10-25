577a59f1ecb82de5e773689aed0ae2befcfed71c
MacBookHiro <>
2020-10-25 13:59:24 +0900

    ListTileを編集

--- lib/views/top_page.dart
+++ lib/views/top_page.dart
@@ -11,13 +11,37 @@ class TopPage extends StatelessWidget {
         child: ListView(
           children: [
             ListTile(
-              title: Text('list 1'),
+              title: Text('name 1 (ticker 1)'),
+              subtitle: Text('market 1'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+                  IconButton(icon: Icon(Icons.delete), onPressed: () {}),
+                ],
+              ),
             ),
             ListTile(
-              title: Text('list 2'),
+              title: Text('name 2 (ticker 2)'),
+              subtitle: Text('market 2'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+                  IconButton(icon: Icon(Icons.delete), onPressed: () {}),
+                ],
+              ),
             ),
             ListTile(
-              title: Text('list 3'),
+              title: Text('name 3 (ticker 3)'),
+              subtitle: Text('market 3'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+                  IconButton(icon: Icon(Icons.delete), onPressed: () {}),
+                ],
+              ),
             ),
           ]
         ),
