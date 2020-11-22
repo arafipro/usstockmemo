f2870a5be9442eacad5854aeec46c6b3a38e077c
MacBookHiro <>
2020-11-22 17:24:28 +0900

    システム側で変更

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:usstock_name/main.dart';
+import 'package:usstockmemo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
