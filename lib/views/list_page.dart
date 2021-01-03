c7311c039e985513c907b93739b4578e0cfb94e5
MacBookHiro <>
2021-01-03 09:31:00 +0900

    Card内のフォーマットを変更

--- lib/views/list_page.dart
+++ lib/views/list_page.dart
@@ -16,64 +16,93 @@ class ListPage extends StatelessWidget {
             final listTiles = memos
                 .map(
                   (memo) => Card(
-                    child: ListTile(
-                      title: Row(
-                        children: [
-                          Text(
-                            memo.name,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            '(' + memo.ticker.toUpperCase() + ')',
-                          ),
-                        ],
-                      ),
-                      subtitle: Text(memo.market.toUpperCase()),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () async {
-                              await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => EditPage(
-                                    stockmemo: memo,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        ListTile(
+                          title: Column(
+                            children: [
+                              Text(
+                                memo.name,
+                                textAlign: TextAlign.center,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    '(' + memo.ticker.toUpperCase() + ')',
+                                    textAlign: TextAlign.center,
                                   ),
-                                  fullscreenDialog: true,
-                                ),
-                              );
-                              model.fetchMemos();
-                            },
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    memo.market.toUpperCase(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  // SizedBox(
+                                  //   width: 8,
+                                  // ),
+                                  // Text('登録日時')
+                                ],
+                              ),
+                            ],
                           ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () async {
-                              await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: Text('${memo.name}を削除しますか？'),
-                                    actions: <Widget>[
-                                      FlatButton(
-                                        child: Text('OK'),
-                                        onPressed: () async {
-                                          Navigator.of(context).pop();
-                                          await deleteMemo(
-                                              context, model, memo);
-                                        },
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                          ),
-                        ],
-                      ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text(memo.memo),
+                        ),
+                        ButtonBar(
+                          alignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            RaisedButton.icon(
+                              color: Colors.blue,
+                              shape: StadiumBorder(),
+                              icon: Icon(Icons.edit),
+                              label: Text('編集'),
+                              onPressed: () async {
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EditPage(
+                                      stockmemo: memo,
+                                    ),
+                                    fullscreenDialog: true,
+                                  ),
+                                );
+                                model.fetchMemos();
+                              },
+                            ),
+                            RaisedButton.icon(
+                              color: Colors.red,
+                              shape: StadiumBorder(),
+                              icon: Icon(Icons.delete),
+                              label: Text('削除'),
+                              onPressed: () async {
+                                await showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text('${memo.name}を削除しますか？'),
+                                      actions: <Widget>[
+                                        FlatButton(
+                                          child: Text('OK'),
+                                          onPressed: () async {
+                                            Navigator.of(context).pop();
+                                            await deleteMemo(
+                                                context, model, memo);
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 )
